22819738416006e95be660e55c837d0c0811fd20
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2026-05-07 09:59:55 +0200

    Rename `Ordered` to `TopoOrdered` in topological sort extensions for better clarity

--- TopoSort/TopologicalSortExtensions.cs
+++ TopoSort/TopologicalSortExtensions.cs
@@ -11,12 +11,12 @@ public static class TopologicalSortExtensions
     extension<T>(IEnumerable<ValueTuple<T, T>> dependencies)
         where T : notnull
     {
-        public IEnumerable<T> Ordered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
+        public IEnumerable<T> TopoOrdered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
     }
 
     extension<T>(IEnumerable<Tuple<T, T>> dependencies)
         where T : notnull
     {
-        public IEnumerable<T> Ordered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
+        public IEnumerable<T> TopoOrdered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
     }
 }
