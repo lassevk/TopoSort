0f384223a5d079ed260b4ea7b9de7a815fee1a87
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2026-05-07 13:23:04 +0200

    Move `TopologicalSortVertexExtensions` into `TopologicalSortExtensions` for consistency and API simplification.

--- TopoSort/TopologicalSortExtensions.cs
+++ TopoSort/TopologicalSortExtensions.cs
@@ -1,7 +1,7 @@
 namespace TopoSort;
 
 /// <summary>
-/// Adds extension methods to IEnumerable to perform topological sorting.
+/// Adds extension methods to IEnumerable to perform topological sorting, and for creating edges.
 /// </summary>
 public static class TopologicalSortExtensions
 {
@@ -101,5 +101,53 @@ public static class TopologicalSortExtensions
         /// </exception>
         public IEnumerable<T> TopoOrdered(IEqualityComparer<T>? equalityComparer = null, IComparer<T>? comparer = null)
             => TopologicalSorter.Ordered(edges.Select(d => new TopologicalSortEdge<T>(d.Item1, d.Item2)), equalityComparer, comparer);
+    }
+
+    /// <param name="dependent">The vertex value that the edge will point to.</param>
+    /// <typeparam name="T">The type of the values of the vertices of the created edge.</typeparam>
+    extension<T>(T dependent)
+    {
+        /// <summary>
+        /// Creates a <see cref="TopologicalSortEdge{T}"/> with the value as the dependent.
+        /// </summary>
+        /// <param name="dependency">
+        /// The dependency for the vertex.
+        /// </param>
+        /// <returns>
+        /// A <see cref="TopologicalSortEdge{T}"/> that represents an edge dependency from
+        /// <paramref name="dependency"/> to dependent.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// 10.DependsOn(5)
+        /// </code>
+        ///
+        /// produces a dependency on the form 5 --&gt; 10.
+        /// </example>
+        public TopologicalSortEdge<T> DependsOn(T dependency) => new(dependency, dependent);
+    }
+
+    /// <param name="dependency">The vertex value that the edge will originate from.</param>
+    /// <typeparam name="T">The type of the values of the vertices of the created edge.</typeparam>
+    extension<T>(T dependency)
+    {
+        /// <summary>
+        /// Creates a <see cref="TopologicalSortEdge{T}"/> with the value as the dependency.
+        /// </summary>
+        /// <param name="dependent">
+        /// The dependency for the vertex.
+        /// </param>
+        /// <returns>
+        /// A <see cref="TopologicalSortEdge{T}"/> that represents an edge dependency from
+        /// dependency to <paramref name="dependent"/>.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// 5.WithDependent(10)
+        /// </code>
+        ///
+        /// produces a dependency on the form 5 --&gt; 10.
+        /// </example>
+        public TopologicalSortEdge<T> WithDependent(T dependent) => new(dependency, dependent);
     }
 }
