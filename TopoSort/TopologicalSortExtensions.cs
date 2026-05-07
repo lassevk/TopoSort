7e60601d8ea01f3fd80600af343894f9bd855144
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2026-05-07 10:00:47 +0200

    Add optional IEqualityComparer parameter to TopologicalSort extension methods

--- TopoSort/TopologicalSortExtensions.cs
+++ TopoSort/TopologicalSortExtensions.cs
@@ -5,18 +5,18 @@ public static class TopologicalSortExtensions
     extension<T>(IEnumerable<TopologicalSortDependency<T>> dependencies)
         where T : notnull
     {
-        public IEnumerable<T> Ordered() => new TopologicalSorter().Ordered(dependencies);
+        public IEnumerable<T> Ordered(IEqualityComparer<T>? comparer = null) => new TopologicalSorter().Ordered(dependencies, comparer);
     }
 
     extension<T>(IEnumerable<ValueTuple<T, T>> dependencies)
         where T : notnull
     {
-        public IEnumerable<T> TopoOrdered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
+        public IEnumerable<T> TopoOrdered(IEqualityComparer<T>? comparer = null) => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)), comparer);
     }
 
     extension<T>(IEnumerable<Tuple<T, T>> dependencies)
         where T : notnull
     {
-        public IEnumerable<T> TopoOrdered() => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)));
+        public IEnumerable<T> TopoOrdered(IEqualityComparer<T>? comparer = null) => new TopologicalSorter().Ordered(dependencies.Select(d => new TopologicalSortDependency<T>(d.Item1, d.Item2)), comparer);
     }
 }
